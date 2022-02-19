c9c0d1b0694e7d8d21b1f088445084d625f4e5f7
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-02-19 18:42:14 +0530

    files added

---
+++ etc/nginx/conf.d
@@ -0,0 +1,11 @@
+FROM node:14-alpine AS builder
+WORKDIR /app
+COPY package.json ./
+COPY yarn.lock ./
+RUN yarn install --frozen-lockfile
+COPY . .
+RUN yarn build
+
+FROM nginx:1.19-alpine AS server
+COPY ./etc/nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=builder ./app/build /usr/share/nginx/html
